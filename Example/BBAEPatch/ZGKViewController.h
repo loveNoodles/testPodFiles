6fd733f872617e7fe0c71256301fe580f917ee02
zhangguangkai <guangkai.zhang@jimubox.com>
2017-03-09 11:49:44 +0800

    a projectQQ

---
+++ Example/BBAEPatch/ZGKViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZGKViewController.h
+//  BBAEPatch
+//
+//  Created by loveNoodles on 03/09/2017.
+//  Copyright (c) 2017 loveNoodles. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ZGKViewController : UIViewController
+
+@end
---
+++ Example/BBAEPatch/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BBAEPatch
+//
+//  Created by loveNoodles on 03/09/2017.
+//  Copyright (c) 2017 loveNoodles. All rights reserved.
+//
+
+@import UIKit;
+#import "ZGKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZGKAppDelegate class]));
+    }
+}
